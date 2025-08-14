78507f9f20f15b4ecbb60a1312451cb2b39f4578
daisyx <daisy1xu4@gmail.com>
2025-08-14 11:13:02 -0700

    Added images for projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,16 +1,31 @@
 import React, {Component} from "react";
 
+import WonderWoman from './../Resources/WonderWoman.png';
+import ProjectSwagger from './../Resources/ProjectSwagger.png';
+import CatNyap from './../Resources/CatNyap.png';
+
+const projectImages = {
+    "wonderwoman": WonderWoman,
+    "projectswagger": ProjectSwagger,
+    "catnyap": CatNyap
+};
+
 class Project extends Component{
     render() {
         return (
             <div key={this.props.data}>
-                {this.props.data.link.length > 0 ? <a href={this.props.data.link}>{this.props.data.name}</a> : <h1>{this.props.data.name}</h1>}
-                <div>Genre: {this.props.data.genre}</div>
-                <div>Platform: {this.props.data.platform}</div>
-                <div>Studio: {this.props.data.studio}</div>
-                <div>Date: {this.props.data.date}</div>
-                <div>Role: {this.props.data.role}</div>
-                <div>Engine: {this.props.data.engine}</div>
+                <div>
+                    <div>
+                        {this.props.data.link.length > 0 ? <a href={this.props.data.link}>{this.props.data.name}</a> : <h1>{this.props.data.name}</h1>}
+                        <div>Genre: {this.props.data.genre}</div>
+                        <div>Platform: {this.props.data.platform}</div>
+                        <div>Studio: {this.props.data.studio}</div>
+                        <div>Date: {this.props.data.date}</div>
+                        <div>Role: {this.props.data.role}</div>
+                        <div>Engine: {this.props.data.engine}</div>
+                    </div>
+                    {this.props.data.image.length > 0 ? <img src={projectImages[this.props.data.image]} alt={this.props.data.title} /> : null}
+                </div>
                 <p>{this.props.data.description}</p>
             </div>
         )
