3f4219e77c70506cad88cd179bc46070b6175c58
daisyx <daisy1xu4@gmail.com>
2025-08-27 17:13:38 -0700

    Grammer fix

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <div>
                 <p>I am a Software Engineer with years of experience working in the Gaming industry. I've worked on both AAA and mobile titles. I have expertise in gameplay and UI programming.</p>
                 <p>I have experience in many aspect of programming, from testing to tools to gameplay. As a result, I am a very strong generalist and am able to jump into any systems. I enjoy working with designers to flesh out and implement their systems. In my free time, I enjoy doing arts and crafts, finding new food to try, and going out to find cool new places.</p>
-                <p>Most Recent title: Wonder Woman (cancelled)</p>
+                <p>Most Recent Title: Wonder Woman (cancelled)</p>
             </div>
         </div>
     );
